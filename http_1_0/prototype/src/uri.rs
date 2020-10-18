bd80755df5bd31a210df67169345de2129e1c77c
JPNYKW <haruto_17n1101083@nnn.ed.jp>
2020-10-19 00:19:12 +0900

    :construction: Parse authority and path (HTTP Scheme)

--- http_1_0/prototype/src/uri.rs
+++ http_1_0/prototype/src/uri.rs
@@ -1,16 +1,30 @@
-mod authority;
-
-use authority::Authority;
-
 pub fn parse_uri(uri_str: impl AsRef<str>) -> URI {
     let uri_str = uri_str.as_ref();
     if uri_str.starts_with("http://") {
         let content = &uri_str["http://".len()..];
-        let content_iter = content.split("@").into_iter();
+        let mut content_iter = content.split("/").into_iter();
+
+        let authority = parse_authority(content_iter.next().expect("Failed to unwrap authority"));
+        let path = parse_path(content_iter.next().expect("Failed to unwrap path"));
+
         todo!()
     } else {
         todo!()
     }
+}
+
+fn parse_authority(target: impl AsRef<str>) -> Authority {
+  let mut content = target.as_ref().split(":").into_iter();
+  let host = content.next().expect("Failed to unwrap host").to_string();
+  let port = content.next().expect("Failed to unwrap port").to_string();
+  Authority { host, port }
+}
+
+fn parse_path(target: impl AsRef<str>) -> Path {
+  let mut content = target.as_ref().split("?").into_iter();
+  let path = content.next().expect("Failed to unwrap host").to_string();
+  let searchpart = content.next().expect("Failed to unwrap port").to_string();
+  Path { path, searchpart }
 }
 
 pub enum URI {
@@ -30,6 +44,15 @@ enum Scheme {
     Other(String),
 }
 
+struct Authority {
+  host: String,
+  port: String,
+}
+
+struct Path {
+  path: String,
+  searchpart: String,
+}
 
 struct Location {
     host: String,
